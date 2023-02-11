6aea250d8e1cdb526735d0882af459ca52a18548
Arun Kumar <46559817+chafoot@users.noreply.github.com>
2023-02-11 23:41:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,6 +5,11 @@ import ast
 import math
 import random
 
+import sys
+from importlib import reload
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
